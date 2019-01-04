549050a770097408e58d00a0b42bc2f2787d4fd5
Alex Norman <alex@x37v.info>
2019-01-04 11:01:25 -0800

    markdown links

--- scripts/makecrates.py
+++ scripts/makecrates.py
@@ -68,18 +68,18 @@ SRC_LIB_RS_TPL = """\
 //! {svd2rust_version})
 //!
 //! You can find an overview of the API here:
-//! https://docs.rs/svd2rust/{svd2rust_version}/svd2rust/#peripheral-api
+//! [svd2rust/#peripheral-api](https://docs.rs/svd2rust/{svd2rust_version}/svd2rust/#peripheral-api)
 //!
 //! For more details see the README here:
-//! https://github.com/stm32-rs/stm32-rs
+//! [stm32-rs](https://github.com/stm32-rs/stm32-rs)
 //!
 //! This crate supports all {family} devices; for the complete list please
 //! see:
-//! https://github.com/stm32-rs/stm32-rs/tree/master/{crate}
+//! [{crate}](https://github.com/stm32-rs/stm32-rs/tree/master/{crate})
 //!
 //! Due to doc build limitations, not all devices may be shown on docs.rs;
 //! a representative few have been selected instead. For a complete list of
-//! available registers and fields see: https://stm32.agg.io/rs
+//! available registers and fields see: [stm32-rs Device Coverage](https://stm32.agg.io/rs)
 
 #![allow(non_camel_case_types)]
 #![no_std]
