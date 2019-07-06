b81fecd4e0d5255a794217eff1c856c8b6e13f76
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2019-07-06 18:37:58 +0100

    stm32h7: Rename stm32h7x3 into a total of 4 devices
    stm32h743/753 with revision v versions
    
    The resulting patched SVDs for stm32h753 (not rev v) is identical to
    the previous output for stm32h7x3. The stm32h743 is almost identical,
    less the CRYP peripheral and interrupts.
    
    A less awful alternative to the symbolic links would be appreciated...
    
    ST have released v1.5 SVDs (we currently have v1.4). However the only
    change in the release is to remove/hide the 7a3x / 7b3x SVDs. So the
    archive is not updated in this PR.

--- svd/extract.sh
+++ svd/extract.sh
@@ -2,3 +2,8 @@
 for f in vendor/*.zip; do
     unzip -juLL $f '*.svd'
 done
+
+ln -s stm32h743x.svd stm32h743.svd || true
+ln -s stm32h743x.svd stm32h743v.svd || true
+ln -s stm32h753x.svd stm32h753.svd || true
+ln -s stm32h753x.svd stm32h753v.svd || true
