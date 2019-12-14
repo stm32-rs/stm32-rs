c78aec6f6fcc7659b0a2f25691bc7dc7e56200c2
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2019-12-14 19:37:54 +0000

    stm32h7: more progress on dual core
    - Add *cm4 target. This has identical registers to the *cm7
      equivalent, but different interrupts
    - Fix error in RCC GCR relating to C-M4 core
    - Add dualcore parts of DBGMCU peripheral
    - Add dualcore parts of EXTI peripheral
    - Fix EXTI register access for dual clore
    
    Still TODO
    
    - Add dualcore parts of SYSCFG

--- stm32h7/build.rs
+++ stm32h7/build.rs
@@ -9,12 +9,16 @@ fn main() {
             "src/stm32h743/device.x"
         } else if env::var_os("CARGO_FEATURE_STM32H743V").is_some() {
             "src/stm32h743v/device.x"
+        } else if env::var_os("CARGO_FEATURE_STM32H747CM4").is_some() {
+            "src/stm32h747cm4/device.x"
         } else if env::var_os("CARGO_FEATURE_STM32H747CM7").is_some() {
             "src/stm32h747cm7/device.x"
         } else if env::var_os("CARGO_FEATURE_STM32H753").is_some() {
             "src/stm32h753/device.x"
         } else if env::var_os("CARGO_FEATURE_STM32H753V").is_some() {
             "src/stm32h753v/device.x"
+        } else if env::var_os("CARGO_FEATURE_STM32H757CM4").is_some() {
+            "src/stm32h757cm4/device.x"
         } else if env::var_os("CARGO_FEATURE_STM32H757CM7").is_some() {
             "src/stm32h757cm7/device.x"
         } else { panic!("No device features selected"); };
