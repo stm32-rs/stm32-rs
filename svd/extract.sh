0122f3ecff1ea710b4dbeab71a7b736190958f83
Pawel Chojnacki <pawel.chcki@gmail.com>
2021-02-02 12:36:27 +0100

    Update stm32g4 svd zip to newest download from STM site
    New SVD ZIP contains fixes and missing svd file for stm32g491 chip
    
    $ curl -s -L -o- https://www.st.com/resource/en/svd/stm32g4_svd.zip | sha1sum
    d9ad831ef1001a1760f90a8ec6456f824c25525f  -

--- svd/extract.sh
+++ svd/extract.sh
@@ -11,3 +11,12 @@ cp stm32h7x5_cm4.svd stm32h747cm4.svd
 cp stm32h7x5_cm7.svd stm32h747cm7.svd
 cp stm32h7b3x.svd stm32h7b3.svd
 cp stm32mp157x.svd stm32mp157.svd
+cp stm32g4a1xx.svd stm32g4a1.svd
+cp stm32g431xx.svd stm32g431.svd
+cp stm32g441xx.svd stm32g441.svd
+cp stm32g471xx.svd stm32g471.svd
+cp stm32g473xx.svd stm32g473.svd
+cp stm32g474xx.svd stm32g474.svd
+cp stm32g483xx.svd stm32g483.svd
+cp stm32g484xx.svd stm32g484.svd
+cp stm32g491xx.svd stm32g491.svd
