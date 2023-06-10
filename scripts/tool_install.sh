07aa86f09ad9a08aa80f8e1657f29b6040eb3d8d
eZio Pan <eziopan@qq.com>
2023-06-10 09:49:47 +0800

    check CARGO_HOME env var

--- scripts/tool_install.sh
+++ scripts/tool_install.sh
@@ -29,26 +29,29 @@ case "${1:-}" in
         ;;
 esac
 
+# check CARGO_HOME env var
+CARGO_BIN_DIR="${CARGO_HOME:-~/.cargo/bin}"
+
 if [ "${form:-}" ]; then
     echo "form = ${form}"
-    curl -sSfL https://github.com/djmcgill/form/releases/download/$form/form-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/form
-    chmod +x ~/.cargo/bin/form
+    curl -sSfL https://github.com/djmcgill/form/releases/download/$form/form-x86_64-unknown-linux-gnu.gz | gzip -d - > "${CARGO_BIN_DIR}/form"
+    chmod +x "${CARGO_BIN_DIR}/form"
 fi
 
 if [ "${svdtools:-}" ]; then
     echo "svdtools = ${svdtools}"
-    curl -sSfL https://github.com/stm32-rs/svdtools/releases/download/$svdtools/svdtools-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/svdtools
-    chmod +x ~/.cargo/bin/svdtools
+    curl -sSfL https://github.com/stm32-rs/svdtools/releases/download/$svdtools/svdtools-x86_64-unknown-linux-gnu.gz | gzip -d - > "${CARGO_BIN_DIR}/svdtools"
+    chmod +x "${CARGO_BIN_DIR}/svdtools"
 fi
 
 if [ "${svd2rust:-}" ]; then
     echo "svd2rust = ${svd2rust}"
-    curl -sSfL https://github.com/rust-embedded/svd2rust/releases/download/$svd2rust/svd2rust-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/svd2rust
-    chmod +x ~/.cargo/bin/svd2rust
+    curl -sSfL https://github.com/rust-embedded/svd2rust/releases/download/$svd2rust/svd2rust-x86_64-unknown-linux-gnu.gz | gzip -d - > "${CARGO_BIN_DIR}/svd2rust"
+    chmod +x "${CARGO_BIN_DIR}/svd2rust"
 fi
 
 if [ "${svd2html:-}" ]; then
     echo "svd2html = ${svd2html}"
-    curl -sSfL https://github.com/burrbull/svd2html/releases/download/$svd2html/svd2html-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/svd2html
-    chmod +x ~/.cargo/bin/svd2html
+    curl -sSfL https://github.com/burrbull/svd2html/releases/download/$svd2html/svd2html-x86_64-unknown-linux-gnu.gz | gzip -d - > "${CARGO_BIN_DIR}/svd2html"
+    chmod +x "${CARGO_BIN_DIR}/svd2html"
 fi
