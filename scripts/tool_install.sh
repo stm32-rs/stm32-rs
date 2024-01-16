510368bd91e5bcb7a4c856a5ee0fe8560cdf0913
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2024-01-16 23:08:13 +0100

    tools_install: support $CARGO_HOME environment variable
    See [Cargo Home](https://doc.rust-lang.org/cargo/guide/cargo-home.html) in the
    Cargo Book

--- scripts/tool_install.sh
+++ scripts/tool_install.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+CARGO_HOME="${CARGO_HOME:-$HOME/.cargo/}"
+
 FORM_VERSION="${FORM_VERSION:-v0.10.0}"
 SVDTOOLS_VERSION="${SVDTOOLS_VERSION:-v0.3.8}"
 SVD2RUST_VERSION="${SVD2RUST_VERSION:-v0.31.5}"
@@ -31,24 +33,24 @@ esac
 
 if [ "${form:-}" ]; then
     echo "form = ${form}"
-    curl -sSfL https://github.com/djmcgill/form/releases/download/$form/form-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/form
-    chmod +x ~/.cargo/bin/form
+    curl -sSfL https://github.com/djmcgill/form/releases/download/$form/form-x86_64-unknown-linux-gnu.gz | gzip -d - > $CARGO_HOME/bin/form
+    chmod +x $CARGO_HOME/bin/form
 fi
 
 if [ "${svdtools:-}" ]; then
     echo "svdtools = ${svdtools}"
-    curl -sSfL https://github.com/stm32-rs/svdtools/releases/download/$svdtools/svdtools-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/svdtools
-    chmod +x ~/.cargo/bin/svdtools
+    curl -sSfL https://github.com/stm32-rs/svdtools/releases/download/$svdtools/svdtools-x86_64-unknown-linux-gnu.gz | gzip -d - > $CARGO_HOME/bin/svdtools
+    chmod +x $CARGO_HOME/bin/svdtools
 fi
 
 if [ "${svd2rust:-}" ]; then
     echo "svd2rust = ${svd2rust}"
-    curl -sSfL https://github.com/rust-embedded/svd2rust/releases/download/$svd2rust/svd2rust-x86_64-unknown-linux-gnu.gz | gzip -d - > ~/.cargo/bin/svd2rust
-    chmod +x ~/.cargo/bin/svd2rust
+    curl -sSfL https://github.com/rust-embedded/svd2rust/releases/download/$svd2rust/svd2rust-x86_64-unknown-linux-gnu.gz | gzip -d - > $CARGO_HOME/bin/svd2rust
+    chmod +x $CARGO_HOME/bin/svd2rust
 fi
 
 if [ "${svdconv:-}" ]; then
     echo "svdconv = ${svdconv}"
-    curl -sSfL https://github.com/Open-CMSIS-Pack/devtools/releases/download/tools/svdconv/$svdconv/svdconv-$svdconv-linux-amd64.tbz2 | tar -xj -C ~/.cargo/bin/
-    chmod +x ~/.cargo/bin/svdconv
+    curl -sSfL https://github.com/Open-CMSIS-Pack/devtools/releases/download/tools/svdconv/$svdconv/svdconv-$svdconv-linux-amd64.tbz2 | tar -xj -C $CARGO_HOME/bin/
+    chmod +x $CARGO_HOME/bin/svdconv
 fi
